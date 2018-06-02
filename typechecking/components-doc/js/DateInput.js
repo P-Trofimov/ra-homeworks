966224ac21a3d322674d369558b5d96825f01923
Pavel Trofimov <trofimovpavelv@gmail.com>
2018-06-02 21:43:01 +0300

    Исправление замечаний, перенос из кодпена

--- typechecking/components-doc/js/DateInput.js
+++ typechecking/components-doc/js/DateInput.js
@@ -9,3 +9,25 @@ const DateInput = props => {
     </div>
   )
 };
+
+const today = new Date();
+DateInput.defaultProps = {
+  value: today.toISOString().slice(0, 10)
+};
+
+const datePropType = (props, propName, componentName) => {
+  let date = props[propName];
+  let isDate = (typeof date === 'string') && /^(\d){4}-(\d){2}-(\d){2}$/.test(date);
+  if(!isDate) {
+    return new Error(`Неверный параметр ${propName} в компоненте ${componentName}: параметр должен быть датой в формате YYYY-MM-DD`);
+  }
+  return null;
+};
+
+DateInput.propTypes = {
+  label: PropTypes.string,
+  name: PropTypes.string,
+  required: PropTypes.bool,
+  onChange: PropTypes.func,
+  value: datePropType
+};
